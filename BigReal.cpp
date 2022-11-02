3a13e44965577e22d1f18786b6eb9441851db70a
callmejoee <92326279+callmejoee@users.noreply.github.com>
2022-11-02 17:29:46 +0200

    Test1

--- BigReal.cpp
+++ BigReal.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     BigDecimalInt a("123456789012345678901234567890");
-    BigDecimalInt b("987654321098765432109876543210");
+    BigDecimalInt b("9876543210987654321098765432109");
     BigDecimalInt c = a + b;
     cout << c << endl;
     return 0;
