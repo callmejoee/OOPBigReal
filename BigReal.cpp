cd31da97cf02f6e403a49f8cf001b268771cc207
callmejoee <92326279+callmejoee@users.noreply.github.com>
2022-11-05 00:31:35 +0200

    First 3 functions

--- BigReal.cpp
+++ BigReal.cpp
@@ -44,8 +44,38 @@ public:
         fraction = BigDecimalInt(fraction_string);
 
     };
-    BigReal(string realNumber);
-    BigReal(BigDecimalInt bigInteger);
+    BigReal(string realNumber){
+        // check if number is negative and remove sign
+        if (realNumber[0] == '-') {
+            sign = '-';
+            realNumber.erase(0, 1);
+        }
+        else if (realNumber[0] == '+') {
+            realNumber.erase(0, 1);
+        }
+
+        // split number into whole and fraction
+        int j = 0;
+        for(int i = 0; i < realNumber.length(); i++){
+            if( j == 0 && realNumber[i] != '.'){
+                whole_string += realNumber[i];
+            }
+            else if(realNumber[i] == '.'){
+                j = 1;
+            }
+            else if(j == 1){
+                fraction_string += realNumber[i];
+            }
+
+        }
+        // convert string to BigDecimalInt
+        whole = BigDecimalInt(whole_string);
+        fraction = BigDecimalInt(fraction_string);
+    };
+    BigReal(BigDecimalInt bigInteger){
+        whole = bigInteger;
+        fraction = BigDecimalInt("0");
+    };
 };
 int main() {
     BigReal a(-123.456);
