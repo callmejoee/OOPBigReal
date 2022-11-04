a9d4bcd4973e5c9177d7e2d08bd1e5a3882d4b16
callmejoee <92326279+callmejoee@users.noreply.github.com>
2022-11-05 00:23:59 +0200

    First function

--- BigReal.cpp
+++ BigReal.cpp
@@ -12,8 +12,10 @@ private:
     char sign = '+';
 public:
     BigReal(double realNumber = 0.0){
+
         // convert number to string
         number = to_string(realNumber);
+
         // check if number is negative and remove sign
         if (number[0] == '-') {
             sign = '-';
@@ -22,6 +24,7 @@ public:
         else if (number[0] == '+') {
             number.erase(0, 1);
         }
+
         // split number into whole and fraction
         int j = 0;
         for(int i = 0; i < number.length(); i++){
